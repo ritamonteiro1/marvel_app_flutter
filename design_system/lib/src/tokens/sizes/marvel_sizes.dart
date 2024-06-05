90c7e267a24f278b23a5f7c3c3f843f5d6c784ea
ritamonteiro1 <ritaalvesmonteiro@gmail.com>
2024-06-04 23:21:15 -0300

    feat: create sizes tokens

---
+++ design_system/lib/src/tokens/sizes/marvel_sizes.dart
@@ -0,0 +1,25 @@
+abstract class MarvelBorder {
+  static const x1 = 1.0;
+  static const x2 = 2.0;
+}
+
+abstract class MarvelRadius {
+  static const x10 = 16.00;
+}
+
+abstract class MarvelSpacing {
+  static const x50 = 2.00;
+  static const x100 = 4.00;
+  static const x150 = 6.00;
+  static const x200 = 8.00;
+  static const x250 = 12.00;
+  static const x300 = 16.00;
+  static const x350 = 20.00;
+  static const x400 = 24.00;
+  static const x450 = 28.00;
+  static const x500 = 32.00;
+  static const x550 = 40.00;
+  static const x600 = 48.00;
+  static const x650 = 48.00;
+  static const x700 = 56.00;
+}
