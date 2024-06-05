418cd04294844e08f3a2e23e8f07bf3e438ae205
ritamonteiro1 <ritaalvesmonteiro@gmail.com>
2024-06-04 23:28:46 -0300

    remove: create basic structure of WidgetBookMarvel

---
+++ widgetbook_marvel/lib/src/card_primary_component.dart
@@ -0,0 +1,52 @@
+import 'package:design_system/design_system.dart';
+import 'package:flutter/material.dart';
+import 'package:widgetbook/widgetbook.dart';
+
+class CardPrimaryComponent extends WidgetbookComponent {
+  CardPrimaryComponent()
+      : super(
+          name: 'Card Primary',
+          useCases: [
+            WidgetbookUseCase(
+              name: 'Horizontal',
+              builder: (context) {
+                return Scaffold(
+                  body: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(MarvelSpacing.x300),
+                      child: CardPrimary(
+                        imageUrl:
+                            'http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg',
+                        text: '3-D Man',
+                        onClick: () {},
+                        imageWidth: 200,
+                        imageHeight: 200,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+            WidgetbookUseCase(
+              name: 'Vertical',
+              builder: (context) {
+                return Scaffold(
+                  body: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(MarvelSpacing.x300),
+                      child: CardPrimary.vertical(
+                        imageUrl:
+                            'http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg',
+                        text: '3-D Man',
+                        onClick: () {},
+                        imageWidth: 200,
+                        imageHeight: 200,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
+        );
+}
---
+++ widgetbook_marvel/lib/src/components.dart
@@ -0,0 +1,1 @@
+export 'card_primary_component.dart';
