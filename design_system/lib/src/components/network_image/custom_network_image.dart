49e7e4d5cd097c63da6c5707fb48cd382bd8ad72
ritamonteiro1 <ritaalvesmonteiro@gmail.com>
2024-06-04 23:22:06 -0300

    feat: create CustomNetworkImage

---
+++ design_system/lib/src/components/network_image/custom_network_image.dart
@@ -0,0 +1,41 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class CustomNetworkImage extends StatelessWidget {
+  const CustomNetworkImage({
+    super.key,
+    required this.imageUrl,
+    required this.loadingProgressColor,
+    required this.errorIconColor,
+    required this.height,
+    required this.width,
+  });
+
+  final String imageUrl;
+  final Color loadingProgressColor;
+  final Color errorIconColor;
+  final double height;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      width: width,
+      height: height,
+      imageUrl: imageUrl,
+      fit: BoxFit.fill,
+      progressIndicatorBuilder: (context, url, downloadProgress) => Center(
+        child: CircularProgressIndicator(
+          value: downloadProgress.progress,
+          color: loadingProgressColor,
+        ),
+      ),
+      errorWidget: (context, url, error) => Center(
+        child: Icon(
+          Icons.error,
+          color: errorIconColor,
+        ),
+      ),
+    );
+  }
+}
