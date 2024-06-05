ee96d181487ce1206aa900cb5dbea2b4a56c5075
ritamonteiro1 <ritaalvesmonteiro@gmail.com>
2024-06-05 20:26:28 -0300

    feat: generated app name with intl

--- internationalization/lib/internationalization.dart
+++ internationalization/lib/internationalization.dart
@@ -1,7 +1,5 @@
 library internationalization;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:flutter_localizations/flutter_localizations.dart';
+
+export './src/generated/l10n.dart';
---
+++ internationalization/lib/src/generated/l10n.dart
@@ -0,0 +1,88 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'intl/messages_all.dart';
+
+// **************************************************************************
+// Generator: Flutter Intl IDE plugin
+// Made by Localizely
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, lines_longer_than_80_chars
+// ignore_for_file: join_return_with_assignment, prefer_final_in_for_each
+// ignore_for_file: avoid_redundant_argument_values, avoid_escaping_inner_quotes
+
+class MarvelStrings {
+  MarvelStrings();
+
+  static MarvelStrings? _current;
+
+  static MarvelStrings get current {
+    assert(_current != null,
+        'No instance of MarvelStrings was loaded. Try to initialize the MarvelStrings delegate before accessing MarvelStrings.current.');
+    return _current!;
+  }
+
+  static const AppLocalizationDelegate delegate = AppLocalizationDelegate();
+
+  static Future<MarvelStrings> load(Locale locale) {
+    final name = (locale.countryCode?.isEmpty ?? false)
+        ? locale.languageCode
+        : locale.toString();
+    final localeName = Intl.canonicalizedLocale(name);
+    return initializeMessages(localeName).then((_) {
+      Intl.defaultLocale = localeName;
+      final instance = MarvelStrings();
+      MarvelStrings._current = instance;
+
+      return instance;
+    });
+  }
+
+  static MarvelStrings of(BuildContext context) {
+    final instance = MarvelStrings.maybeOf(context);
+    assert(instance != null,
+        'No instance of MarvelStrings present in the widget tree. Did you add MarvelStrings.delegate in localizationsDelegates?');
+    return instance!;
+  }
+
+  static MarvelStrings? maybeOf(BuildContext context) {
+    return Localizations.of<MarvelStrings>(context, MarvelStrings);
+  }
+
+  /// `Marvel App`
+  String get app_name {
+    return Intl.message(
+      'Marvel App',
+      name: 'app_name',
+      desc: '',
+      args: [],
+    );
+  }
+}
+
+class AppLocalizationDelegate extends LocalizationsDelegate<MarvelStrings> {
+  const AppLocalizationDelegate();
+
+  List<Locale> get supportedLocales {
+    return const <Locale>[
+      Locale.fromSubtags(languageCode: 'pt', countryCode: 'BR'),
+    ];
+  }
+
+  @override
+  bool isSupported(Locale locale) => _isSupported(locale);
+  @override
+  Future<MarvelStrings> load(Locale locale) => MarvelStrings.load(locale);
+  @override
+  bool shouldReload(AppLocalizationDelegate old) => false;
+
+  bool _isSupported(Locale locale) {
+    for (var supportedLocale in supportedLocales) {
+      if (supportedLocale.languageCode == locale.languageCode) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
