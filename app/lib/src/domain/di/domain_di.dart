fdbb07caac20d441ab1d5429679002ec70d1e130
ritamonteiro1 <ritaalvesmonteiro@gmail.com>
2024-06-05 23:40:11 -0300

    feat: inject domain layer

---
+++ app/lib/src/domain/di/domain_di.dart
@@ -0,0 +1,37 @@
+import 'package:get_it/get_it.dart';
+
+import '../repository/marvel_repository.dart';
+import '../use_case/get_character_details/get_character_details_use_case.dart';
+import '../use_case/get_character_details/get_character_details_use_case_impl.dart';
+import '../use_case/get_character_list/get_character_list_use_case.dart';
+import '../use_case/get_character_list/get_character_list_use_case_impl.dart';
+import '../use_case/get_favorite_characters/get_favorite_characters_use_case.dart';
+import '../use_case/get_favorite_characters/get_favorite_characters_use_case_impl.dart';
+import '../use_case/toggle_character_favorite/toggle_character_favorite_use_case.dart';
+import '../use_case/toggle_character_favorite/toggle_character_favorite_use_case_impl.dart';
+
+void domainDI() {
+  final getIt = GetIt.instance;
+
+  getIt
+    ..registerFactory<GetCharacterListUseCase>(
+      () => GetCharacterListUseCaseImpl(
+        repository: getIt.get<MarvelRepository>(),
+      ),
+    )
+    ..registerFactory<GetCharacterDetailsUseCase>(
+      () => GetCharacterDetailsUseCaseImpl(
+        repository: getIt.get<MarvelRepository>(),
+      ),
+    )
+    ..registerFactory<GetFavoriteCharactersUseCase>(
+      () => GetFavoriteCharactersUseCaseImpl(
+        repository: getIt.get<MarvelRepository>(),
+      ),
+    )
+    ..registerFactory<ToggleCharacterFavoriteUseCase>(
+      () => ToggleCharacterFavoriteUseCaseImpl(
+        repository: getIt.get<MarvelRepository>(),
+      ),
+    );
+}
