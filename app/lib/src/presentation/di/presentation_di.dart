95dbd4c4cd5a5fdec26ae6a8a35be9c4114655ef
ritamonteiro1 <ritaalvesmonteiro@gmail.com>
2024-06-05 23:41:55 -0300

    Revert "feat: inject home bloc"
    This reverts commit ed3413efa7d16af1ddb5556b98594ba4032101f3.

--- app/lib/src/presentation/di/presentation_di.dart
+++
@@ -1,9 +0,0 @@
-import 'package:app/src/presentation/bloc/home/home_bloc.dart';
-import 'package:get_it/get_it.dart';
-
-void presentationDI() {
-  final getIt = GetIt.instance;
-  getIt.registerFactory(() => HomeBloc(
-        getCharacterListUseCase: getIt.get(),
-      ));
-}
